21e3d3a4bdfc4e320f8514e7712343e5e34ae4f3
Ismael Ojeda Perez <iojedaperez@gmail.com>
2015-11-16 23:17:35 +0000

    Test for Article

---
+++ src/test/java/main/ArticleTest.java
@@ -0,0 +1,29 @@
+package main;
+
+import org.junit.Test;
+
+/**
+ * Created by Ismael Ojeda Perez on 15/11/2015.
+ */
+public class ArticleTest {
+
+    @Test
+    public void testName() {
+
+    }
+
+    @Test
+    public void testDate() {
+
+    }
+
+    @Test
+    public void testImg() {
+
+    }
+
+    @Test
+    public void testPrice() {
+
+    }
+}
