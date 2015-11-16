1fbeb059cda524c92b8d7c43f483103d5ac8ec39
Ismael Ojeda Perez <iojedaperez@gmail.com>
2015-11-16 23:18:35 +0000

    Test Main Class

---
+++ src/test/java/main/WebExtractorTest.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by Ismael Ojeda Perez on 15/11/2015.
+ */
+public class WebExtractorTest {
+
+    WebExtractor webExtractor = new WebExtractor();
+/*
+    public void startTest() {
+        webExtractor.start();
+    }
+*/
+}
