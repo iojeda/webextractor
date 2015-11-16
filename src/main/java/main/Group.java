66bbef9e5044bb37a605d3328c3531c2fed525f8
Ismael Ojeda Perez <iojedaperez@gmail.com>
2015-11-16 23:15:48 +0000

    Defined Class for Group

---
+++ src/main/java/main/Group.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * Created by Ismael Ojeda Perez on 15/11/2015.
+ */
+public class Group {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
