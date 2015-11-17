28958ddb1af5e8b634bf1b1b2748e8a064c5fbbe
Ismael Ojeda Perez <iojedaperez@gmail.com>
2015-11-17 02:30:07 +0000

    Pending  test implementation

--- src/test/java/main/ArticleTest.java
+++ src/test/java/main/ArticleTest.java
@@ -7,6 +7,15 @@ import org.junit.Test;
  */
 public class ArticleTest {
 
+    String date="";
+    String img="VerImagen('http://cluster.binarycanarias.com/imagen.asp?cp-10103890.jpg'); return false;";
+    String name="Productillo de los malos";
+    String opportunity="";
+    String newArticle="";
+    String unknown="";
+    String price="20.36";
+    String group="";
+    Article article = new Article(date,img,name,opportunity,newArticle,unknown,price,group);
     @Test
     public void testName() {
 
@@ -24,6 +33,51 @@ public class ArticleTest {
 
     @Test
     public void testPrice() {
+
+    }
+
+    @Test
+    public void testGetDate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetImg() throws Exception {
+
+    }
+
+    @Test
+    public void testGetImgUrl() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOpportunity() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNewArticle() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUnknown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPrice() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGroup() throws Exception {
 
     }
 }
